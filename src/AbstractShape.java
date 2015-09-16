a223eb2be7283ea33243fe865e6545c9913e5eb7
Rutger <rutgermaniser@hotmail.com>
2015-09-16 19:46:52 +0200

    Test

---
+++ src/AbstractShape.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+/**
+ * base class AbstractShape
+ *
+ */
+public abstract class AbstractShape extends Object
+
+{
+
+	// Data members
+	protected Point ptStart; // Start point of shape
+
+	protected Point ptEnd; // End point of shape
+
+	protected int nwidth; // Width of shape
+
+	protected int nheight; // Height of shape
+	
+	protected int arcwidth; //Arc width of Rounded Rectangle
+	
+	protected int archeigth; //Arc height of Rounded Rectangle
+
+	protected Color clrFront; // Default color
+
+	public AbstractShape() {
+		// default color is red, try your combinations
+		clrFront = Color.red;
+	}
+
+	public abstract void setStart(Point pt);
+
+	public abstract void setEnd(Point pt);
+
+	public Point getStart() {
+		return ptStart;
+	}
+
+	public Point getEnd() {
+		return ptEnd;
+	}
+
+	public abstract void Draw(Graphics2D g);
+
+}
+
+// Base class cShape ends
